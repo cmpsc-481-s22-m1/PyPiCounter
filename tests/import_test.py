4b220b3d225ee83991fafb6f1fcda67152ab97f8
Adriana Solis <60237877+solisa986@users.noreply.github.com>
2022-01-21 12:41:16 -0500

    ok if this doesnt work kick me out

--- tests/import_test.py
+++ tests/import_test.py
@@ -1,2 +1,6 @@
-import pytest
-import csv
+"""Test case for imports."""
+import csv # pylint: disable=W0611
+import pytest # pylint: disable=W0611
+
+def test_imports():
+    """Test case to test the counting of imports."""
