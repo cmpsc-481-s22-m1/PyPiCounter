6214a6d6f31dc77131cce6f8cc96c20a8e20b6c2
Adriana Solis <60237877+solisa986@users.noreply.github.com>
2022-01-21 12:21:40 -0500

    I accidentally made everything fail that is my bad

--- src/py_counter.py
+++ src/py_counter.py
@@ -3,7 +3,7 @@ import libcst as cst
 
 def PyCount():
     # parses through the given test file
-    search = cst.parse_module(open("tests/test.py").read())
+    search = cst.parse_module(open("tests/import_test.py").read())
 
     def count_class_definitions():
         definitions = m.findall(search, m.ClassDef())
