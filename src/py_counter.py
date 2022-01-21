1a597153c4ce30b32e7cd74e8bb1ba706b102cfa
caldwella2 <caldwella2@allegheny.edu>
2022-01-21 12:07:31 -0500

    added implementation of count_import_statements

---
+++ src/py_counter.py
@@ -0,0 +1,13 @@
+import libcst.matchers as m
+import libcst as cst
+
+def PyCount():
+    # parses through the given test file
+    search = cst.parse_module(open("tests/test.py").read())
+
+    def count_class_definitions():
+        definitions = m.findall(search, m.ClassDef())
+        print(len(definitions))
+        
+    def count_import_statements():
+        print(m.findall(search, m.Import()))
