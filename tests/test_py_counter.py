43bf23face09a702599ab7f841b77366a8f18fd1
RyanHiltyAllegheny <42868768+RyanHiltyAllegheny@users.noreply.github.com>
2022-01-26 10:21:12 -0500

    Update test_py_counter.py

--- tests/test_py_counter.py
+++ tests/test_py_counter.py
@@ -1,7 +1,6 @@
 """This module implements the test cases for the PyCount Program."""
     
 def main():
-    """First if count test""""
     x,y = 6,7
     if (x<y):
         st = "x is less than y"
