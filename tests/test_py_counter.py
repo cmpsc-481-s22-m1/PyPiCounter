5f51dc2ec5996d93dd8a84686807cc253123965a
RyanHiltyAllegheny <42868768+RyanHiltyAllegheny@users.noreply.github.com>
2022-01-26 10:10:26 -0500

    Update test_py_counter.py

---
+++ tests/test_py_counter.py
@@ -0,0 +1,11 @@
+"""This module implements the ability to greet users by name."""
+
+
+def greet(name):
+    """Greet a user by name."""
+    greeting = f"Hello, {name}!"
+    print(greeting)
+
+
+if __name__ == "__main__":
+    greet("World")
