126f7207157e4128e0552e2e947d169319328f4c
solisa986 <solisa@allegheny.edu>
2022-01-27 16:08:13 -0500

    added more counting functions and fixed the class/functions w/o docs

--- src/command_line_interface.py
+++ src/command_line_interface.py
@@ -24,21 +24,39 @@ def main(  # pylint: disable=R0913
     function_without_docstrings: bool = typer.Option(
         False, "--function_without_docstrings"
     ),
+    function_with_docstrings: bool = typer.Option(
+        False, "--function_with_docstrings"
+    ),
+    classes_with_docstrings: bool = typer.Option(
+        False, "--classes_with_docstrings"
+    ),
     class_without_docstrings: bool = typer.Option(False, "--class_without_docstrings"),
 ):
     """Main method to display the different options."""
     pycount = PyCount(input_file)
     if class_def:
         console.print("\n# of class definitions: " + str(pycount.count_class_definitions()))
+
     if import_statements:
         console.print("\n# of import statements: " + str(pycount.count_import_statements()))
+
     if comment:
         console.print("\n# of comments: " + str(pycount.count_comments()))
+
     if function_def:
         console.print("\n# of function definitions: " + str(pycount.count_function_definitions()))
+
     if function_without_docstrings:
         console.print("\n# of functions w/o docstrings: " + str(pycount.count_functions_without_docstring())) # pylint: disable=C0301
+
+    if function_with_docstrings:
+        console.print("\n# of functions with docstrings: " + str(pycount.count_functions_with_docstring())) # pylint: disable=C0301
+
     if class_without_docstrings:
         console.print("\n# of classes w/o docstrings: " + str(pycount.count_classes_without_docstring())) # pylint: disable=C0301
+
+    if classes_with_docstrings:
+        console.print("\n# of functions with docstrings: " + str(pycount.count_classes_with_docstring())) # pylint: disable=C0301
+
     if if_statements:
         console.print("\n# of if statements: " + str(pycount.count_if_statements())) # pylint: disable=C0301
--- src/py_counter.py
+++ src/py_counter.py
@@ -9,44 +9,77 @@ class PyCount:
 
     def __init__(self, filename):
         """Declaring the self variable."""
-        with open(filename, 'a+') as file:
+        with open(filename, 'r') as file:
             self.search = cst.parse_module(file.read())  # pylint: disable=R1732,W1514
 
     def count_class_definitions(self):
         """Counting the class definitions."""
         return len(m.findall(self.search, m.ClassDef()))
 
+
     def count_comments(self):
         """Counting the comments."""
         return len(m.findall(self.search, m.Comment()))
+
 
     def count_import_statements(self):
         """Counting the import statements."""
         return len(m.findall(self.search, m.Import()))
 
+
     def count_if_statements(self):
         """Counting the if statements."""
         return len(m.findall(self.search, m.If()))
+
 
     def count_function_definitions(self):
         """Counting the function definitions."""
         func_definitions = m.findall(self.search, m.FunctionDef())
         return len(func_definitions)
 
+
     def count_functions_without_docstring(self):  # pylint: disable=R1710
         """Counting the function definitions without docstrings."""
         functions_list = m.findall(self.search, m.FunctionDef())
         count = 0
+        total_count = []
         for find in functions_list:
-            if find.get_docstring() is False:
+            if find.get_docstring() is None:
+                total_count.append(count)
                 count += 1
-                return count
+        return count
+
+
+    def count_functions_with_docstring(self):  # pylint: disable=R1710
+        """Counting the function definitions with docstrings."""
+        function_definitions2 = m.findall(self.search, m.FunctionDef())
+        count = 0
+        total = []
+        for finding in function_definitions2:
+            if finding.get_docstring() is not None:
+                count += 1
+                total.append(count)
+        return count
 
     def count_classes_without_docstring(self):  # pylint: disable=R1710
         """Counting the class definitions without docstrings."""
         class_definitions = m.findall(self.search, m.ClassDef())
         count = 0
-        for finding in class_definitions:
-            if finding.get_docstring() is False:
+        total_count = []
+        for find in class_definitions:
+            if find.get_docstring() is None:
+                total_count.append(count)
                 count += 1
-                return count
+        return count
+
+
+    def count_classes_with_docstring(self):  # pylint: disable=R1710
+        """Counting the class definitions without docstrings."""
+        class_definitions2 = m.findall(self.search, m.ClassDef())
+        count = 0
+        total = []
+        for find in class_definitions2:
+            if find.get_docstring() is not None:
+                total_count.append(count)
+                count += 1
+        return count
