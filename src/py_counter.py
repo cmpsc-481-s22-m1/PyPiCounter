2eba708b7815b750f8f7743d838a9785c14ab860
guzmanr04 <42869491+guzmanr04@users.noreply.github.com>
2022-01-27 10:15:54 -0500

    Update py_counter.py

--- src/py_counter.py
+++ src/py_counter.py
@@ -31,9 +31,8 @@ class PyCount:
 
     def count_function_definitions(self):
         """Counting the function definitions."""
-        func_definitions = m.findall(self.search, m.FunctionDef())
-        return len(func_definitions)
-
+        return len(m.findall(self.search, m.FunctionDef()))
+        
     def count_functions_without_docstring(self):  # pylint: disable=R1710
         """Counting the function definitions without docstrings."""
         functions_list = m.findall(self.search, m.FunctionDef())
